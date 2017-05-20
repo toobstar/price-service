e59501b270360fac01a335279d3ecbea995f9892
toby <toby@skaffold.com>
2017-05-20 10:31:15 +1000

    switch JSON model from object to array at top level

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var request = require('request');
 function PriceService() {
     var fetch = function (url, callbackFn) {
 
-        var testStr = '[{"ticker":"NZM", date":"2017-05-18","close":"0.75","high":"0.75","low":"0.73","split":"","volume":45815},{"ticker":"SCP",date":"2017-05-18","close":"2.27","high":"2.29","low":"2.26","split":"","volume":4002044}]';
+        var testStr = '[{"ticker":"NZM", "date":"2017-05-18","close":"0.75","high":"0.75","low":"0.73","split":"","volume":45815},{"ticker":"SCP","date":"2017-05-18","close":"2.27","high":"2.29","low":"2.26","split":"","volume":4002044}]';
         callbackFn(JSON.parse(testStr));
 
         //request(url, function (error, response, body) {
