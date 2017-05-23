167d3fb4bcee8f0be27b97c4deb91571b451dc1f
toby <toby@skaffold.com>
2017-05-24 09:33:29 +1000

    use proper request api

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ var request = require('request');
 function PriceService() {
     var fetch = function (url, callbackFn) {
 
-        var testStr = '[{"ticker":"NZM", "date":"2017-05-18","close":"0.75","high":"0.75","low":"0.73","split":"","volume":45815},{"ticker":"SCP","date":"2017-05-18","close":"2.27","high":"2.29","low":"2.26","split":"","volume":4002044}]';
-        callbackFn(JSON.parse(testStr));
+        //var testStr = '[{"ticker":"NZM", "date":"2017-05-18","close":"0.75","high":"0.75","low":"0.73","split":"","volume":45815},{"ticker":"SCP","date":"2017-05-18","close":"2.27","high":"2.29","low":"2.26","split":"","volume":4002044}]';
+        //callbackFn(JSON.parse(testStr));
 
-        //request(url, function (error, response, body) {
-        //    console.log('error:', error);
-        //    console.log('statusCode:', response && response.statusCode);
-        //    //console.log('body:', body);
-        //    if (response.statusCode == 200) {
-        //        callbackFn(JSON.parse(body));
-        //    }
-        //});
+        request(url, function (error, response, body) {
+            if (error) {
+                console.log('error:', error);
+                return;
+            }
+            else if (response.statusCode == 200) {
+                callbackFn(JSON.parse(body));
+                return;
+            }
+            console.log('error response:', response);
+        });
     };
 
     var obj = {};
